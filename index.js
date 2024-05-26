30804071e610926d14a217fcf843cbf8a12f626c
ilayavrahami <165209264+ilayavrahami@users.noreply.github.com>
2024-05-26 16:28:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function addToCart(product) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push(product);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert('מוצר נוסף לעגלה');
+}
